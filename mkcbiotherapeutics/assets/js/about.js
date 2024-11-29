b586dd5bf2a4c00ec59d8329098d9087c80c0fbe
vibhav <kamathpvk@gmail.com>
2024-11-29 09:10:45 +0530

    about carousel

---
+++ mkcbiotherapeutics/assets/js/about.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+  new Swiper(".carousel1", {
+    // slidesPerView: 3,
+    breakpoints: {
+      320: {
+        slidesPerView: 1, // 1 slide for mobile
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 2, // 2 slides for tablets
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 3, // 3 slides for desktops
+        spaceBetween: 30,
+      },
+    },
+    centeredSlides: false, // Ensures slides align to the start of the container
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});

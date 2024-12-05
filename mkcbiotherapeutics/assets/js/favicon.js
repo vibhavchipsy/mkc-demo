6b9cba690c2be681d54d5ca5b4a1d7744ed06775
shiva <shiva@shiva.com>
2024-12-05 10:33:56 +0530

    favicon js

---
+++ mkcbiotherapeutics/assets/js/favicon.js
@@ -0,0 +1,8 @@
+(function () {
+    const head = document.querySelector('head');
+
+    const faviconLinks = `
+        <link rel="icon" type="image/svg+xml" href="/mkcbiotherapeutics/assets/logo.svg">
+    `;
+    head.insertAdjacentHTML('beforeend', faviconLinks);
+})();

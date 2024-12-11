c784da00a5c751cf968c39d8eb7c0c26bcef1bdf
shiva <shiva@shiva.com>
2024-12-11 16:51:10 +0530

    on click

--- mkcbiotherapeutics/assets/global.js
+++ mkcbiotherapeutics/assets/global.js
@@ -1,3 +1,5 @@
+// import { handleAnchorNavigation } from "./js/utils.js";
+
 // Function to load the header
 function loadHeader() {
   fetch("/components/header.html") // Path to your header file
@@ -27,6 +29,33 @@ function loadFooter() {
     .catch((error) => console.error("Error loading footer:", error));
 }
 
+function handleAnchorNavigation() {
+  // Function to handle anchor link navigation
+  function navigateToSection() {
+    const hash = window.location.hash; // Get the fragment identifier from URL
+
+    if (hash) {
+      document.querySelector("#close_menu").click();
+      const targetElement = document.querySelector(hash);
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }
+
+  // Add event listener to handle navigation on page load
+  document.addEventListener("DOMContentLoaded", () => {
+    navigateToSection();
+  });
+
+  // Add event listener for dynamic hash change
+  window.addEventListener("hashchange", navigateToSection);
+}
+
 // Call the function
 loadHeader();
 loadFooter();
+
+document.addEventListener("DOMContentLoaded", () => {
+  handleAnchorNavigation();
+});
